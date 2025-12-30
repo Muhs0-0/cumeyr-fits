18124aa55e7f3e543285ae59090aec592cdeb718
Muhsin Hassan <muhsoo2025@gmail.com>
2025-12-30 09:36:19 +0300

    added route

--- server/src/index.js
+++ server/src/index.js
@@ -17,7 +17,7 @@ app.use(cors({
     'http://localhost:3000',
     'http://localhost:5174',
     'https://cumeyr-fits-production.up.railway.app',
-    'https://cumeyr-fits.vercel.app'
+    'https://qaf-fits.vercel.app'
   ],
   credentials: true
 }));
